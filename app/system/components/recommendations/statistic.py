dbb178c32ac15eb3e8684ae23e37a5d851c8892c
Evgeniy Radchenko <rey.8d01@yandex.ru>
2014-11-03 22:11:06 +0600

    backend side

---
+++ app/system/components/recommendations/statistic.py
@@ -0,0 +1,416 @@
+__author__ = 'rey'
+
+# Используем JSON для преобразования объекта словаря в текст и обратно
+import json
+from math import sqrt, fabs, floor
+# from similarity import SimilarityDistance, SimilarityPearson, SimilarityJaccard, SimilarityManhattan
+
+
+class Similarity:
+    """ Коэффициенты сходимости """
+
+    def distance(self, source, p1, p2):
+        """
+        Оценка подобия на основе Евклидова расстояния
+
+        :param p1:
+        :param p2:
+        :return:
+        """
+        # Получить список предметов, оцененных обоими
+        si = {}
+        for item in source[p1]:
+            if item in source[p2]:
+                si[item] = 1
+
+        # Если нет ни одной общей оценки вернуть 0
+        # если нет ни одного предмета который оценили бы оба
+        if len(si) == 0: return 0
+
+        # Сложить квадраты разностей
+        sum_of_squares = sum([pow(source[p1][item] - source[p2][item], 2)
+                              for item in source[p1] if item in source[p2]])
+
+        return round(1 / (1 + sum_of_squares), 3)
+
+    def pearson(self, source, p1, p2):
+        """
+        Коэффициент корреляции Пирсона
+
+        :param p1:
+        :param p2:
+        :return:
+        """
+        # Получить список предметов оцененных обоими
+        si = {}
+        for item in source[p1]:
+            if item in source[p2]:
+                si[item] = 1
+
+        # Найти число элементов
+        n = len(si)
+
+        # Если нет ни одной общей оценки, вернуть 0
+        if n == 0:
+            return 0
+
+        # Вычислить сумму всех предпочтений
+        sum1 = sum([source[p1][it] for it in si])
+        sum2 = sum([source[p2][it] for it in si])
+
+        # Вычислить сумму квадратов
+        sum1sq = sum([pow(source[p1][it], 2) for it in si])
+        sum2sq = sum([pow(source[p2][it], 2) for it in si])
+
+        # Вычислить сумму произведений
+        psum = sum([source[p1][it] * source[p2][it] for it in si])
+
+        # Вычислить коэффициент Пирсона
+        num = psum - (sum1 * sum2 / n)
+        den = sqrt((sum1sq - pow(sum1, 2) / n) * (sum2sq - pow(sum2, 2) / n))
+        if den == 0:
+            return 0
+
+        return round(num / den, 3)
+
+    def jaccard(self, source, p1, p2):
+        """
+        Коэффициент Жаккара
+
+        :param p1:
+        :param p2:
+        :return:
+        """
+        # Получить количество предметов оцененных обоими
+        si = 0
+        for item in source[p1]:
+            if item in source[p2]:
+                si = si + 1
+
+        return round(si / (len(source[p1]) + len(source[p2]) - si), 3)
+
+    def manhattan(self, source, p1, p2):
+        """
+        Расстояние городских кварталов (манхэттенское расстояние)
+
+        :param p1:
+        :param p2:
+        :return:
+        """
+        # Получить список предметов, оцененных обоими
+        si = {}
+        for item in source[p1]:
+            if item in source[p2]:
+                si[item] = 1
+
+        # Если нет ни одной общей оценки вернуть 0
+        if len(si) == 0: return 0
+
+        # Сложить модули разностей
+        sum_of_abs = sum([fabs(source[p1][item] - source[p2][item])
+                          for item in source[p1] if item in source[p2]])
+
+        return round(1 / (1 + sum_of_abs), 3)
+
+
+class Statistic(Similarity):
+    """
+    source - набор данных с людьми и их оценками к фильмам
+    person - конкретный человек для которого определяется наилучший критик (человек с похожими оценками) и выработка рекомендации
+    n - количество данных в результате
+    similarity_class - класс функции сходимости
+    """
+
+    TYPE_SOURCE = 'source'
+    TYPE_TRANSFORMS = 'transforms'
+    TYPE_SIMILARITY_ITEMS = 'similarity_items'
+
+    def __init__(self, source):
+        self.source = source
+        self.transforms = self.calculate_source_transforms()
+
+    @property
+    def source(self):
+        return self._source
+
+    @source.setter
+    def source(self, value):
+        self._source = value
+
+    @property
+    def transforms(self):
+        if hasattr(self, '_transforms') is False:
+            self.transforms = self.calculate_source_transforms()
+        return self._transforms
+
+    @transforms.setter
+    def transforms(self, value):
+        self._transforms = value
+
+    @property
+    def similar_items(self):
+        if hasattr(self, '_similar_items') is False:
+            self.similar_items = self.calculate_similar_items()
+        return self._similar_items
+
+    @similar_items.setter
+    def similar_items(self, value):
+        self._similar_items = value
+
+    def calculate_source_transforms(self):
+        """
+        Преобразование людей в товары
+
+        :return:
+        """
+        result = {}
+        for person in self.source:
+            for item in self.source[person]:
+                result.setdefault(item, {})
+
+                # Обменять местами человека и предмет
+                result[item][person] = self.source[person][item]
+
+        return result
+
+    def calculate_similar_items(self, n=10):
+        """
+        Построение набора данных для сравнения образцов
+
+        :param n:
+        :return:
+        """
+        # Создать словарь, содержащий для каждого образца те образцы, которые больше всего похожи на него
+        result = {}
+
+        # Обратить матрицу предпочтений, чтобы строки соответствовали образцам
+        item_source = self.calculate_source_transforms()
+        c = 0
+
+        for item in item_source:
+            # Обновление состояния для больших наборов данных
+            c += 1
+            if c % 100 == 0:
+                print("%d / %d" % (c, len(item_source)))
+            # Найти образцы, максимально похожий на данный
+            scores = self.top_matches(item, n=n, source=item_source, similarity=self.distance)
+            result[item] = scores
+        return result
+
+    def top_matches(self, person, n=5, source=TYPE_SOURCE, similarity=None):
+        """
+        Возвращает список наилучших соответствий для человека из словаря source
+        - наиболее похожего человека мнением которого будем в последствии оперировать для выработки рекомендации
+        Количество результатов в списке и функция подобия - необязательные параметры
+
+        :param person:
+        :param n:
+        :param source:
+        :param similarity:
+        :return:
+        """
+
+        source = source if isinstance(source, dict) else getattr(self, source, self.source)
+        similarity = similarity or self.pearson
+
+        # Сравнение person со всеми остальными (критиками) по одной из определенных метрик
+        scores = [(similarity(source, person, other), other)
+                  for other in source if other != person]
+        # Отсортировать список по убыванию оценок
+        scores.sort()
+        scores.reverse()
+
+        return scores[0:n]
+
+
+class Recomendations(Statistic):
+
+    def get_recommendations(self, person, n=5, source=Statistic.TYPE_SOURCE, similarity=None):
+        """
+        Получить рекомендации для заданного человека, пользуясь взвешенным средним оценок,
+        данных всеми остальными пользователями
+
+        :param source:
+        :param person:
+        :param n:
+        :param similarity:
+        :return:
+        """
+
+        source = source if isinstance(source, dict) else getattr(self, source, self.source)
+        similarity = self.pearson if similarity is None else similarity
+
+        totals = {}
+        sim_sums = {}
+
+        for other in source:
+            # Сравнивать пользователя с собой же не нужно
+            if other == person:
+                continue
+            sim = similarity(source, person, other)
+
+            # Игнорировать нулевые и отрицательные оценки
+            if sim <= 0:
+                continue
+
+            for item in source[other]:
+                # Оценивать только то что нет среди оценок пользователя person
+                if item not in source[person] or source[person][item] == 0:
+                    # Коэффициент подибия * Оценка
+                    totals.setdefault(item, 0)
+                    totals[item] += source[other][item] * sim
+                    # Сумма коэффициентов подобия
+                    sim_sums.setdefault(item, 0)
+                    sim_sums[item] += sim
+
+        # Создать нормализованный список
+        rankings = [(round(total / sim_sums[item], 3), item) for item, total in totals.items()]
+
+        # Вернуть отсортированный список
+        rankings.sort()
+        rankings.reverse()
+        return rankings[0:n]
+
+    def get_recommendations_transforms(self, person, n=5, similarity=None):
+        self.get_recommendations(person, n, source=Statistic.TYPE_TRANSFORMS, similarity=similarity)
+
+    # def get_recommendations_items(self, source, item_match, user):
+    def get_recommendations_items(self, user):
+        """
+        Выдача рекомендаций на основе сравнения образцов
+
+        :param user:
+        :return:
+        """
+        source = self.source
+        item_match = self.similar_items
+
+        user_ratings = source[user]
+        scores = {}
+        total_sim = {}
+
+        # Цикл по образцам, оцененным данным пользователем
+        for (item, rating) in user_ratings.items():
+
+            # Цикл по образцам похожий на данный
+            for (similarity, item2) in item_match[item]:
+
+                # Пропускаем если пользователь оценил данный образец
+                if item2 in user_ratings:
+                    continue
+
+                # Взвешенная суммы оценок, умноженных на коэффициент подобия
+                scores.setdefault(item2, 0)
+                scores[item2] += similarity * rating
+
+                # Сумма всех коэффициентов подобия
+                total_sim.setdefault(item2, 0)
+                total_sim[item2] += similarity
+
+        # Делим каждую итоговую оценку на взвешенную сумму, чтобы вычислить среднее
+        rankings = [(score / total_sim[item], item) for item, score in scores.items()]
+
+        # Вовзвращает список rankings, отсортированный по убыванию
+        rankings.sort()
+        rankings.reverse()
+        return rankings
+
+
+# Словарь кинокитиков и выставленных ими оценок для небольшого набора данных о фильмах
+critics = {
+    "Lisa Rose": {
+        "Lady in the Water": 2.5,
+        "Snakes on a Plane": 3.5,
+        "Just My Luck": 3.0,
+        "Superman Returns": 3.5,
+        "You, Me, and Dupree": 2.5,
+        "The Night Listener": 3.0},
+    "Gene Seymour": {
+        "Lady in the Water": 3.0,
+        "Snakes on a Plane": 3.5,
+        "Just My Luck": 1.5,
+        "Superman Returns": 5.0,
+        "The Night Listener": 3.0,
+        "You, Me, and Dupree": 3.5},
+    "Michael Phillips": {
+        "Lady in the Water": 2.5,
+        "Snakes on a Plane": 3.0,
+        "Superman Returns": 3.5,
+        "The Night Listener": 4.0},
+    "Claudia Puig": {
+        "Snakes on a Plane": 3.5,
+        "Just My Luck": 3.0,
+        "The Night Listener": 4.5,
+        "Superman Returns": 4.0,
+        "You, Me, and Dupree": 2.5},
+    "Mick LaSalle": {
+        "Lady in the Water": 3.0,
+        "Snakes on a Plane": 4.0,
+        "Just My Luck": 2.0,
+        "Superman Returns": 3.0,
+        "The Night Listener": 3.0,
+        "You, Me, and Dupree": 2.0},
+    "Jack Matthews": {
+        "Lady in the Water": 3.0,
+        "Snakes on a Plane": 4.0,
+        "The Night Listener": 3.0,
+        "Superman Returns": 5.0,
+        "You, Me, and Dupree": 3.5},
+    "Toby": {
+        "Snakes on a Plane": 4.5,
+        "You, Me, and Dupree": 1.0,
+        "Superman Returns": 4.0}
+}
+
+# Тестовый прогон
+# Lisa Rose
+# Gene Seymour
+# Michael Phillips
+# Claudia Puig
+# Mick LaSalle
+# Jack Matthews
+# Toby
+test1 = 'Toby'
+test2 = 'Gene Seymour'
+
+# movies = Statistic.calculate_source_transforms(critics)
+movie = 'You, Me, and Dupree'
+
+my_stat = Recomendations(critics)
+
+# Для выработки рекомендации по образцам набор данных можно строить заранее и использовать его при необходимости,
+# в отличие от способа фильтрации по пользователям, который нужно пересчитывать постоянно при рекомендации.
+# В больших массивах данных вкусы людей будут перекрываться очень слабо,
+# поэтому предпочтительно сравнивать схожесть образцов
+# (которая существенно меняется реже при больших объемах) и выдавать рекомендацию основываясь на ней
+
+# try:
+#     f = open('item_sim.data')
+#     j = json.JSONDecoder()
+#     item_sim = j.decode(f.read())
+#     print('old calculate_similar_items')
+# except Exception as e:
+#     f = open('item_sim.data', 'w')
+#     item_sim = Statistic.calculate_similar_items(critics)
+#     j = json.JSONEncoder()
+#     f.write(j.encode(item_sim))
+#     print('new calculate_similar_items')
+
+print(
+    'Люди:', test1, 'и', test2, '\n',
+    'Евклидово расстояние		', my_stat.distance(my_stat.source, test1, test2), '\n',
+    'Корреляця Пирсона			', my_stat.pearson(my_stat.source, test1, test2), '\n',
+    'Коэффициент Жаккара		', my_stat.jaccard(my_stat.source, test1, test2), '\n',
+    'Манхэттенское расстояние	', my_stat.manhattan(my_stat.source, test1, test2), '\n',
+    '\n',
+    'Ранжирование критиков		', my_stat.top_matches(test1, 2, my_stat.TYPE_SOURCE, my_stat.pearson), '\n',
+    'Выработка рекомендации		', my_stat.get_recommendations(test1), '\n',
+    '\n',
+    'Фильмы похожие на 			', movie, my_stat.top_matches(movie, 3, my_stat.TYPE_TRANSFORMS, my_stat.pearson), '\n',
+    'Кто еще не смотрел фильм	', movie, my_stat.get_recommendations_transforms(movie), '\n',
+    '\n',
+    'Похожие фильмы	\n			', my_stat.similar_items, '\n',
+    'Выработка рекомендации	по образцам	', my_stat.get_recommendations_items(test1), '\n',
+)
+
+
