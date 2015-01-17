fb5f4c872aad3b1c036003fea8155c7ebc147c47
Evgeniy Radchenko <rey.8d01@yandex.ru>
2015-01-17 23:20:55 +0500

    frontend side

--- www/public/system/modules/recommendation/recommendation.js
+++ www/public/system/modules/recommendation/recommendation.js
@@ -6,18 +6,65 @@ chimera.system.recommendation = angular.module("recommendation", ["ngResource", 
 
 chimera.system.recommendation.controller("RecommendationController", ["$scope", "$state", "recommendationService", "omdbapiService",
     function ($scope, $state, recommendationService, omdbapiService) {
-        omdbapiService.search({}, function (response) {
-            console.log(response);
+        console.log("RecommendationController");
+
+        var $input = $('.typeahead');
+
+        $scope.selectItem = {
+            imdb: null,
+            rate: null,
+            year: null,
+            title: null
+        };
+        //Title: "Re-cycle"Type: "movie"Year: "2006"imdbID: "tt0498311"
+
+        $input.typeahead({
+            source: function (s, cb) {
+                var matches = [];
+                $('.typeahead').parent().removeClass("has-error");
+
+                omdbapiService.search({s:s}, function (response) {
+                    console.log(response);
+
+                    if(response.Error) {
+                        $('.typeahead').parent().addClass("has-error");
+                    } else {
+                        for(var item in response.Search) {
+                            matches.push({
+                                imdb: response.Search[item].imdbID,
+                                year: response.Search[item].Year,
+                                title: response.Search[item].Title,
+                                name: response.Search[item].Title
+                            });
+                        }
+                    }
+
+                    cb(matches);
+                });
+
+            },
+            afterSelect: function (item) {
+                $scope.selectItem.imdb = item.imdb;
+                $scope.selectItem.year = item.year;
+                $scope.selectItem.title = item.title;
+            },
+            autoSelect: true
         });
-        //postService.get({aliasPost: $state.params.aliasPost}, function(response) {
-        //    $scope.post = response.content;
-        //});
+
+        // Установка оценки
+        $scope.setRate = function(rate) {
+            $scope.selectItem.rate = rate;
+            recommendationService.post($scope.selectItem, function (response) {
+
+            });
+        }
+
     }
 ]);
 
 chimera.system.recommendation.factory("recommendationService", ["$resource",
     function ($resource) {
-        //return $resource(chimera.config.baseUrl + "/post/:aliasPost");
+        return $resource(chimera.config.baseUrl + "/harvest");
     }
 ]);
 
@@ -25,7 +72,7 @@ chimera.system.recommendation.factory("omdbapiService", ["$resource",
     function ($resource) {
         var urlApi = "//www.omdbapi.com";
         return $resource(urlApi, {}, {
-            "search": {'method': "GET", isArray: false, url: urlApi+"/?s=matrix"}
+            "search": {'method': "GET", isArray: false, url: urlApi + "/?s=:s", params: {s:''}}
         });
     }
 ]);
