6c0ec207cd0c6618a7bf414c11a2f68622c653be
rey <rey.8d01@yandex.ru>
2015-10-20 01:38:36 +0500

    refactoring

--- app/system/handler.py
+++ app/system/handler.py
@@ -1,90 +1,70 @@
-"""
+"""Группа основных обработчиков запросов по выводу результатов работы, авторизации в системе, обработки исключений и т.д.
+
 Основной обработчик запросов торнадо tornado.web.RequestHandler от которого наследуем все остальные обработчики
 через базовый BaseHandler в котором воплощены основные используемые функции.
 
-GET — получение ресурса
-POST — создание ресурса
-PUT — обновление ресурса
-DELETE — удаление ресурса
+GET — получение;
+POST — создание;
+PUT — обновление;
+DELETE — удаление;
 """
-
 import tornado.web
-from tornado.gen import coroutine
 import tornado.escape
-
-from system.utils.result_message import ResultMessage
-
 import system.utils.exceptions
+from tornado.gen import coroutine
 from documents.user import UserDocument, UserOAuthDocument, UserMetaDocument
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    """
-    BaseHandler - основной перекрытый обработчик от которого наследовать все остальные
-
-    воплотить базовые функции для всех потомков
-    """
+    """BaseHandler - основной перекрытый обработчик от которого наследовать все остальные."""
     escape = None
-    result = None
 
     def initialize(self):
-        """
-        Инициализация базового обработчика запросов.
-        """
+        """Инициализация базового обработчика запросов."""
         self.escape = tornado.escape
-        self.result = ResultMessage()
 
     def on_finish(self):
+        """Завершение обработки запроса.
+
+        Не может редактировать содержимое выводимого результата (не может ничего отправлять пользователю в принципе).
+
         """
-        Завершение обработки запроса. Не может редактировать содержимое выводимого результата
-        (не может ничего отправлять пользователю в принципе).
-        """
-        pass
 
     def write_error(self, status_code, **kwargs):
-        """
-        Перехват ошибок и других событий возникающих через исключения.
+        """Перехват ошибок и других событий возникающих через исключения.
 
         :param status_code:
         :param kwargs:
-        :return:
         """
         exc_info = kwargs['exc_info']
         type_exception = exc_info[0]
         exception = exc_info[1]
         traceback = exc_info[0]
 
-        # Попытка привести исключение к строковому виду
+        # Попытка привести исключение к строковому виду.
         if isinstance(exception, system.utils.exceptions.Result):
             # Успешная отработка запроса
             self.set_status(200)
             result = str(exception)
         elif isinstance(exception, system.utils.exceptions.ChimeraException):
-            # Относительно успешная отработка запроса - исключение которое не было правильно обработано
+            # Относительно успешная отработка запроса - исключение которое не было правильно обработано.
             result = str(exception)
         else:
-            # Системное исключение которое было возбуждено феймворком - попытка отработать его корректно для клиента
+            # Системное исключение которое было возбуждено феймворком - попытка отработать его корректно для клиента.
             result_message = system.utils.exceptions.ResultMessage(error=str(exception))
             result = str(result_message)
 
-        # Вывод результата обработки исключения
+        # Вывод результата обработки исключения.
         self.write(result)
         self.finish()
 
     def set_default_headers(self):
-        """
-        Перекрытый метод установки ряда стандартных заголовков
-
-        :return:
-        """
+        """Перекрытый метод установки ряда стандартных заголовков."""
         self.set_header('Content-Type', 'application/json; charset="utf-8"')
         self.set_header('Access-Control-Allow-Origin', '*')
 
     def get_current_user(self):
-        """
-        Перекрытый метод определения пользователя
-        :return:
-        """
+        """Перекрытый метод определения пользователя."""
         user_data = self.get_secure_cookie("chimera_user")
         if user_data:
             return self.escape.json_decode(user_data)
@@ -93,9 +73,7 @@ class BaseHandler(tornado.web.RequestHandler):
 
     @coroutine
     def get_data_current_user(self):
-        """
-        Вернуть данные из базы по текущему пользователю
-        """
+        """Вернет данные из базы по текущему пользователю."""
         user_data = self.get_current_user()
 
         document_user = UserDocument()
@@ -108,28 +86,20 @@ class BaseHandler(tornado.web.RequestHandler):
 
 
 class MainHandler(BaseHandler):
-    """
-    Главный обработчик наследники которого требуют авторизацию со стороны пользователя для своих действий
-    """
+    """Главный обработчик наследники которого требуют авторизацию со стороны пользователя для своих действий."""
 
     def prepare(self):
-        """
-        Перекрытие срабатывает перед вызовом обработчиков и в случае отсутствия данных по пользователю возбуждает исключение.
-        """
+        """Перекрытие срабатывает перед вызовом обработчиков и в случае отсутствия данных по пользователю возбуждает исключение."""
         if self.current_user is None:
             raise system.utils.exceptions.ErrorResult(error="Неизвестный пользователь")
 
 
 class PrivateIntroduceHandler(BaseHandler):
-    """
-    Вход по парольной фразе для привелегированного пользователя
-    """
+    """Вход по парольной фразе для привелегированного пользователя."""
 
     @coroutine
     def post(self):
-        """
-        Авторизация по парольной фразе
-        """
+        """Авторизация по парольной фразе."""
         import bcrypt
 
         hash = '$2a$15$jsDxfdO6tL1gVLoUVZh4AuyBRg92e.sjYY/kA2xKSGM.0MBU7smSq'  # passphrase
@@ -146,17 +116,11 @@ class PrivateIntroduceHandler(BaseHandler):
 
 
 class IntroduceHandler(BaseHandler):
-    """
-    Класс через который будет проводится представление пользователя системе, прошедшего авторизацию
-    """
+    """Класс через который будет проводится представление пользователя системе, прошедшего авторизацию."""
 
     def _load_user_from_post(self, auth_type, user_id):
-        """
-        Сборка документа пользователя для его сохранения
-
-        Вынесли всякое добро в отдельный метод, может потом с изменением архитектуры удобнее это будет вообще
-        держать подальше
-        """
+        """Сборка документа пользователя для его сохранения."""
+        # todo Вынесли всякое добро в отдельный метод, может потом с изменением архитектуры удобнее это будет вообще держать подальше
         document_user = UserDocument()
         document_user_oauth = UserOAuthDocument()
         document_user_meta = UserMetaDocument()
@@ -179,9 +143,7 @@ class IntroduceHandler(BaseHandler):
 
     @coroutine
     def post(self):
-        """
-        Авторизация
-        """
+        """Авторизация."""
         # Основные данные это тип соцсети и ид в ней
         auth_type = self.get_argument("auth_type")
         user_id = self.get_argument("user_id")
@@ -204,9 +166,8 @@ class IntroduceHandler(BaseHandler):
 
 
 class LogoutHandler(MainHandler):
-    """
-    Класс для выхода из системы - очистка кук
-    """
+    """Класс для выхода из системы - очистка кук."""
 
     def get(self):
+        """Сброс авторизации."""
         self.clear_cookie("chimera_user")
--- app/system/utils/exceptions.py
+++ app/system/utils/exceptions.py
@@ -6,65 +6,7 @@
 обработчики) должны возвращать один и тот же, по своей структуре, результат.
 
 """
-import tornado.escape
-
-
-class ResultMessage:
-    """Класс в котором собирается ответ для клиента в виде json и возвращается через специальный обработчик исключений.
-
-    Ответ должен содержать в себе всю необходимую информмацию для возбуждения адекватной реакции клиента:
-    - список ошибок возникших в результате обработки запроса;
-    - запрошенный контент;
-    - необходимые действия для обслуживания клиента;
-
-    :type _message: dict Формируемый ответ в виде определенной структуры;
-    """
-
-    _message = None
-
-    def __init__(self, error: str = None, content: dict = None):
-        """При инициализации происходит описание формата ответа и занесение его в свойство класса.
-
-        :param error:
-        :type error: str
-        :param content:
-        :type content: dict
-        """
-
-        self._message = {
-            "error": error,
-            "content": content if content is not None else {},
-            "maintenance": {
-                "redirect": None,
-                "refresh": None,
-                "delay": None,
-                "cookie": None
-            }
-        }
-
-    def __str__(self):
-        """При обращении к результирующему ответу как к строке - ответ преобразуется в json."""
-        return tornado.escape.json_encode(self._message)
-
-    def update_content(self, data):
-        """Обновление словаря с результирующими данными.
-
-        :param data: Передаваемые данные могут быть любого типа, с которым не возникнет проблем при сериализации в json;
-        """
-        self._message["content"].update(data)
-
-    def set_cookie(self, cookie: str):
-        # todo ref
-        # self._message["maintenance"]["cookie"] = str(cookie)[len("Set-Cookie: chimera_user="):]
-        self._message["maintenance"]["cookie"] = str(cookie)[len("Set-Cookie: "):]
-
-    def get_message(self) -> dict:
-        """Вернет весь подготовленный ответ целиком.
-
-        :return:
-        :rtype: dict
-        """
-        return self._message
+from system.utils.result import ResultMessage
 
 
 class ChimeraException(Exception):
